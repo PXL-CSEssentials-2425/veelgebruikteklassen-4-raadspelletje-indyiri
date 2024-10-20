e1a5699c3953da7611ffee4da104b87823cfd199
Indy Iri <indy.iri@student.pxl.be>
2024-10-20 23:23:28 +0200

    wpf layout opnieuw gemaakt

--- H4Oef4Raadspelletje/H4Oef4Raadspelletje/MainWindow.xaml.cs
+++ H4Oef4Raadspelletje/H4Oef4Raadspelletje/MainWindow.xaml.cs
@@ -36,38 +36,7 @@ namespace H4Oef4Raadspelletje
 
         private void startButton_Click(object sender, RoutedEventArgs e)
         {
-            
-            /*
-            string inputNumber = numberTextBox.Text;
-            bool isInputNumberValid = int.TryParse(inputNumber, out number);
 
-            if (isInputNumberValid && number <= 100 && number >= 0)
-            {
-
-                if (number < randomNumber)
-                {
-                    output1TextBox.Text = "Raad hoger!";
-                }
-                if (number > randomNumber)
-                {
-                    output1TextBox.Text = "Raad lager";
-                }
-                if (number == randomNumber)
-                {
-                    output1TextBox.Text = "Proficiat! U hebt het getal geraden";
-                }
-                amountOfGuesses++;
-                output2TextBox.Text = $"Aantal keer geraden: {amountOfGuesses}";
-                numberTextBox.Focus();
-            }
-            else
-            {
-                output1TextBox.Text = "Geef een getal in tussen 1 en 100";
-                output2TextBox.Text = "Geef een getal in tussen 1 en 100";
-                numberTextBox.Focus();
-                numberTextBox.Clear();
-            }
-            */
         }
     }
 }
