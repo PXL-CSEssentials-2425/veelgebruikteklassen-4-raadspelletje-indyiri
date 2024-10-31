8751809cdd1b8d148d7de9fb8cd0b5c188dbc6d3
Indy Iri <indy.iri@student.pxl.be>
2024-10-31 15:21:12 +0100

    Afgewerkt

--- H4Oef4Raadspelletje/H4Oef4Raadspelletje/MainWindow.xaml.cs
+++ H4Oef4Raadspelletje/H4Oef4Raadspelletje/MainWindow.xaml.cs
@@ -31,31 +31,36 @@ namespace H4Oef4Raadspelletje
             int amountOfGuesses = 0;
             int randomNumber = rnd.Next(0, 101);
 
-            string input = Interaction.InputBox("Geef een getal tussen 0 en 100", "Raadspel");
-            bool isInputValid = int.TryParse(input, out number);
-
             while (number != randomNumber)
             {
+                string input = Interaction.InputBox("Geef een getal tussen 0 en 100", "Raadspel");
+                bool isInputValid = int.TryParse(input, out number);
 
-                if (string.IsNullOrEmpty(input) || !isInputValid)
+                if (string.IsNullOrEmpty(input))
                 {
                     MessageBox.Show("Geef een getal in!", "Foutieve invoer", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                }
+                else if (!isInputValid)
+                {
+                    MessageBox.Show("Ongeldige invoer! Geef een getal in!", "Foutieve invoer", MessageBoxButton.OK, MessageBoxImage.Exclamation);
                 }
                 else if (number < 0 || number > 100)
                 {
                     MessageBox.Show("Geef een getal in tussen 0 en 100!", "Foutieve invoer", MessageBoxButton.OK, MessageBoxImage.Exclamation);
                 }
-                else if (number < randomNumber)
+                else 
                 {
-                    MessageBox.Show("Raad hoger!");
                     amountOfGuesses++;
-                }
-                else
-                {
-                    MessageBox.Show("Raad lager!");
-                    amountOfGuesses++;
-                }                    
-                input = Interaction.InputBox("Geef een getal tussen 0 en 100", "Raadspel");
+
+                    if (number < randomNumber)
+                    {
+                        MessageBox.Show("Raad hoger!");
+                    }
+                    else if (number > randomNumber)
+                    {
+                        MessageBox.Show("Raad lager!");
+                    }
+                }               
             }
             MessageBox.Show($"Proficiat! U hebt het getal geraden! Aantal keer geraden: {amountOfGuesses}", "Gewonnen");
         }
